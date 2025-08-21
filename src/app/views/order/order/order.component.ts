0ce3707debc338c7f3286224a7f895ce7628a5ad
Evgenii Kin <loli.timkin@yandex.ru>
2025-08-21 05:00:37 +0300

    валидация форм + остальной функционал стр заказа + popup

--- src/app/views/order/order/order.component.ts
+++ src/app/views/order/order/order.component.ts
@@ -1,15 +1,116 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {CartService} from "../../../shared/services/cart.service";
+import {CartType} from "../../../../types/cart.type";
+import {DefaultResponseType} from "../../../../types/default-response.type";
+import {Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {DeliveryType} from "../../../../types/delivery.type";
+import {FormBuilder, Validators} from "@angular/forms";
+import {PaymentType} from "../../../../types/payment.type";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
+
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  deliveryType: DeliveryType = DeliveryType.delivery
+  cart: CartType | null = null;
+  totalAmount: number = 0;
+  totalCount: number = 0;
+  deliveryTypes = DeliveryType
+  paymentTypes = PaymentType
 
-  ngOnInit(): void {
+  orderForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    phone: ['', Validators.required],
+    fatherName: [''],
+    paymentType: [PaymentType.cashToCourier, Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    street: [''],
+    house: [''],
+    entrance: [''],
+    apartment: [''],
+    comment: ['']
+  }
+)
+
+  @ViewChild('popup') popup!: TemplateRef<ElementRef>
+  dialogRef: MatDialogRef<any> | null = null;
+
+  constructor(
+    private cartService: CartService,
+    private router: Router,
+    private _snackBar: MatSnackBar,
+    private fb: FormBuilder,
+    private dialog: MatDialog
+  ) {
+    this.updateDeliveryTypeValidation()
   }
 
+  ngOnInit(): void {
+    this.cartService.getCart()
+      .subscribe((data: CartType | DefaultResponseType) => {
+        if((data as DefaultResponseType).error !== undefined) {
+          throw new Error((data as DefaultResponseType).message);
+        }
+        this.cart = data as CartType;
+        if(!this.cart || this.cart  && this.cart.items.length === 0) {
+          this._snackBar.open('Ваша корзина пуста');
+          this.router.navigate(['/'])
+          return;
+        }
+        this.calculateTotal()
+      })
+  }
+
+  calculateTotal() {
+    this.totalAmount = 0;
+    this.totalCount = 0;
+    if(this.cart) {
+      this.cart.items.forEach(item => {
+        this.totalAmount += item.quantity * item.product.price;
+        this.totalCount += item.quantity
+      })
+    }
+  }
+
+  changeDeliveryType(type: DeliveryType) {
+    this.deliveryType = type;
+    this.updateDeliveryTypeValidation()
+
+  }
+
+  updateDeliveryTypeValidation() {
+    if(this.deliveryType === DeliveryType.delivery) {
+      this.orderForm.get('street')?.setValidators(Validators.required);
+      this.orderForm.get('house')?.setValidators(Validators.required);
+    } else {
+      this.orderForm.get('street')?.removeValidators(Validators.required);
+      this.orderForm.get('house')?.removeValidators(Validators.required);
+      this.orderForm.get('street')?.setValue('')
+      this.orderForm.get('house')?.setValue('')
+      this.orderForm.get('entrance')?.setValue('')
+      this.orderForm.get('apartment')?.setValue('')
+    }
+
+    this.orderForm.get('street')?.updateValueAndValidity();
+    this.orderForm.get('house')?.updateValueAndValidity();
+  }
+
+  createOrder() {
+    //if(this.orderForm.valid) {
+      //console.log(this.orderForm.value);
+    this.dialogRef = this.dialog.open(this.popup);
+    //}
+  }
+
+  closePopup() {
+    this.dialogRef?.close();
+    this.router.navigate(['/'])
+  }
 }
