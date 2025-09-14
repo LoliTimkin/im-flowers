7099a337dd4304b0da904d1ff1453d04a6559bf0
Evgenii Kin <loli.timkin@yandex.ru>
2025-09-14 09:25:54 +0300

    урок 13,задание 1

--- src/app/views/personal/favorite/favorite.component.ts
+++ src/app/views/personal/favorite/favorite.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, Output} from '@angular/core';
 import {FavoriteService} from "../../../shared/services/favorite.service";
 import {FavoriteType} from "../../../../types/favorite.type";
 import {DefaultResponseType} from "../../../../types/default-response.type";
 import {environment} from "../../../../environments/environment";
+import {CartType} from "../../../../types/cart.type";
+import {CartService} from "../../../shared/services/cart.service";
+import {ProductType} from "../../../../types/product";
 
 @Component({
   selector: 'app-favorite',
@@ -10,23 +13,55 @@ import {environment} from "../../../../environments/environment";
   styleUrls: ['./favorite.component.scss']
 })
 export class FavoriteComponent implements OnInit {
-
+  cart: CartType | null = null;
+  count: number = 1;
   products: FavoriteType[] = [];
   serverStaticPath: string = environment.serverStaticPath;
 
-  constructor(private favoriteService: FavoriteService) { }
+  constructor(private favoriteService: FavoriteService,
+              private cartService: CartService) { }
 
   ngOnInit(): void {
 
-    this.favoriteService.getFavorites()
-      .subscribe((data: FavoriteType[] | DefaultResponseType) => {
+    this.cartService.getCart()
+      .subscribe((data: CartType | DefaultResponseType) => {
         if((data as DefaultResponseType).error !== undefined) {
-          const error = (data as DefaultResponseType).message ;
-          throw new Error(error)
+          throw new Error((data as DefaultResponseType).message);
         }
+        this.cart = data as CartType;
 
-        this.products = data as FavoriteType[];
-    })
+        this.favoriteService.getFavorites()
+          .subscribe((data: FavoriteType[] | DefaultResponseType) => {
+            if((data as DefaultResponseType).error !== undefined) {
+              const error = (data as DefaultResponseType).message ;
+              throw new Error(error)
+            }
+            const favorites: FavoriteType[] = data as FavoriteType[];
+
+            if(favorites && favorites.length > 0) {
+              this.proccessFavorites(favorites)
+            }
+          })
+      })
+
+  }
+
+  proccessFavorites(favorites: FavoriteType[])  {
+
+    if(this.cart && this.cart.items.length > 0) {
+      this.products = favorites.map(product => {
+        if(this.cart) {
+          const productInCart = this.cart.items.find(item => item.product.id === product.id);
+          if(productInCart) {
+            product.countInCart = productInCart.quantity;
+            //this.count = productInCart.quantity
+          }
+        }
+        return product
+      });
+    } else {
+      this.products = favorites
+    }
   }
 
   removeFromFavorites(id: string) {
@@ -44,5 +79,40 @@ export class FavoriteComponent implements OnInit {
 
 
   }
+
+  addToCart(id: string) {
+    this.cartService.updateCart(id, this.count)
+      .subscribe((data: CartType | DefaultResponseType) => {
+        if((data as DefaultResponseType).error !== undefined) {
+          throw new Error((data as DefaultResponseType).message);
+        }
+
+      })
+  }
+
+  updateCount(id: string, count: number) {
+    if (count) {
+      this.count = count;
+      this.cartService.updateCart(id, this.count)
+        .subscribe((data: CartType | DefaultResponseType) => {
+          if((data as DefaultResponseType).error !== undefined) {
+            throw new Error((data as DefaultResponseType).message);
+          }
+        })
+    }
+  }
+
+  removeFromCart(product: FavoriteType) {
+    this.cartService.updateCart(product.id, 0)
+      .subscribe((data: CartType | DefaultResponseType) => {
+        if((data as DefaultResponseType).error !== undefined) {
+          throw new Error((data as DefaultResponseType).message);
+        }
+        delete product.countInCart;
+        //this.count = 1;
+      })
+  }
+
+
 
 }
